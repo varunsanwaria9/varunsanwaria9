873b8d152c5d9c21341cc1e5a5d35b9a3054a49b
Varun Sanwaria <varunsanwaria9@gmail.com>
2023-08-19 01:38:57 +0530

    Added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/Navbar.css'
+
+export default function Navbar() {
+    return (
+        <nav className="nav-main">
+            <button>Home</button>
+            <button>Work</button>
+            <button>Skills</button>
+            <button>Project</button>
+            <button>Contact</button>
+        </nav>
+)}
