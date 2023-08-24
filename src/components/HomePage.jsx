e834d116c507e0d96b8ca1e3886ba66b6da30d8c
Varun Sanwaria <varunsanwaria9@gmail.com>
2023-08-25 01:04:57 +0530

    Removed navbar and updated files

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from "./Navbar";
 import "../styles/Homepage.css"
 
 export default function HomePage() {
     return (
         <div className="main-body">
-            <Navbar />
             <div className="home-main-body">
                 <div className="home-left-body">
                     <p>Hi, I am</p>
