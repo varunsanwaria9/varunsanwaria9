8fb7f5fb8da666926052572d92270ebae4621dd2
Varun Sanwaria <varunsanwaria9@gmail.com>
2023-08-27 04:16:30 +0530

    Updated navbar to be sticky

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import HomePage from "./components/HomePage";
+import NavbarFn from "./components/Navbar";
+import Work from "./components/Work";
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <NavbarFn />
       <HomePage />
+      <Work />
     </div>
   );
 }
